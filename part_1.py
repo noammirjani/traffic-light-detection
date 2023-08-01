5da710f497dc0b9bcab3d5f19c35441f976c8d65
yehudaheller <116564740+yehudaheller@users.noreply.github.com>
2023-08-01 16:52:44 +0300

    Color recognition, red and green

---
+++ part_1.py
@@ -0,0 +1,153 @@
+from typing import List, Optional, Union, Dict, Tuple
+import json
+import argparse
+from pathlib import Path
+
+import numpy as np
+from scipy import signal as sg
+from scipy.ndimage import maximum_filter
+from PIL import Image
+import matplotlib.pyplot as plt
+
+import cv2
+# if you wanna iterate over multiple files and json, the default source folder name is this.
+DEFAULT_BASE_DIR: str = 'INSERT_YOUR_DIR_WITH_PNG_AND_JSON_HERE'
+
+# The label we wanna look for in the polygons json file
+TFL_LABEL = ['traffic light']
+
+POLYGON_OBJECT = Dict[str, Union[str, List[int]]]
+RED_X_COORDINATES = List[int]
+RED_Y_COORDINATES = List[int]
+GREEN_X_COORDINATES = List[int]
+GREEN_Y_COORDINATES = List[int]
+
+
+
+def find_tfl_lights(c_image: np.ndarray, **kwargs) -> Tuple[list, list, list, list]:
+    # Convert the image to the HSV color space for better color detection
+    hsv_image = cv2.cvtColor(c_image, cv2.COLOR_RGB2HSV)
+
+    # Define the lower and upper thresholds for red and green colors in HSV space
+    lower_red = np.array([0, 100, 100])
+    upper_red = np.array([10, 255, 255])
+
+    # Define the lower and upper thresholds for green colors in HSV space
+    # Fine-tune these values based on the specific green color of traffic lights
+    lower_green = np.array([40, 100, 100])
+    upper_green = np.array([80, 255, 255])
+
+    # Create masks for red and green colors using the specified thresholds
+    red_mask = cv2.inRange(hsv_image, lower_red, upper_red)
+    green_mask = cv2.inRange(hsv_image, lower_green, upper_green)
+
+    # Apply maximum_filter to the green mask to enhance the green areas
+    green_mask = maximum_filter(green_mask, size=100)  # Adjust the size as needed
+
+   # red_mask = maximum_filter(red_mask, size=100)  # Adjust the size as needed
+
+
+    # Use morphological operations to clean up the green mask and remove noise
+    kernel = np.ones((5, 5), np.uint8)  # kernel size 5*5 of all 1
+    green_mask = cv2.morphologyEx(green_mask, cv2.MORPH_CLOSE, kernel)
+
+    # Find contours in the masks to get the coordinates of the red and green areas
+    # Use cv2.RETR_TREE to find all contours and reconstruct the full hierarchy
+    red_contours, _ = cv2.findContours(red_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    green_contours, _ = cv2.findContours(green_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Extract the x and y coordinates of the red and green areas
+    red_x = [int(np.mean(contour[:, :, 0])) for contour in red_contours]
+    red_y = [int(np.mean(contour[:, :, 1])) for contour in red_contours]
+    green_x = [int(np.mean(contour[:, :, 0])) for contour in green_contours]
+    green_y = [int(np.mean(contour[:, :, 1])) for contour in green_contours]
+
+    return red_x, red_y, green_x, green_y
+
+
+
+### GIVEN CODE TO TEST YOUR IMPLENTATION AND PLOT THE PICTURES
+def show_image_and_gt(c_image: np.ndarray, objects: Optional[List[POLYGON_OBJECT]], fig_num: int = None):
+    # ensure a fresh canvas for plotting the image and objects.
+    plt.figure(fig_num).clf()
+    # displays the input image.
+    plt.imshow(c_image)
+    labels = set()
+    if objects:
+        for image_object in objects:
+            # Extract the 'polygon' array from the image object
+            poly: np.array = np.array(image_object['polygon'])
+            # Use advanced indexing to create a closed polygon array
+            # The modulo operation ensures that the array is indexed circularly, closing the polygon
+            polygon_array = poly[np.arange(len(poly)) % len(poly)]
+            # gets the x coordinates (first column -> 0) anf y coordinates (second column -> 1)
+            x_coordinates, y_coordinates = polygon_array[:, 0], polygon_array[:, 1]
+            color = 'r'
+            plt.plot(x_coordinates, y_coordinates, color, label=image_object['label'])
+            labels.add(image_object['label'])
+        if 1 < len(labels):
+            # The legend provides a visual representation of the labels associated with the plotted objects.
+            # It helps in distinguishing different objects in the plot based on their labels.
+            plt.legend()
+
+
+def test_find_tfl_lights(image_path: str, image_json_path: Optional[str]=None, fig_num=None):
+    """
+    Run the attention code.
+    """
+    # using pillow to load the image
+    image: Image = Image.open(image_path)
+    # converting the image to a numpy ndarray array
+    c_image: np.ndarray = np.array(image)
+
+    objects = None
+    if image_json_path:
+        image_json = json.load(Path(image_json_path).open())
+        objects: List[POLYGON_OBJECT] = [image_object for image_object in image_json['objects']
+                                         if image_object['label'] in TFL_LABEL]
+
+    show_image_and_gt(c_image, objects, fig_num)
+
+    red_x, red_y, green_x, green_y = find_tfl_lights(c_image)
+    # 'ro': This specifies the format string. 'r' represents the color red, and 'o' represents circles as markers.
+    plt.plot(red_x, red_y, 'ro', markersize=4)
+    plt.plot(green_x, green_y, 'go', markersize=4)
+
+
+def main(argv=None):
+    """
+    It's nice to have a standalone tester for the algorithm.
+    Consider looping over some images from here, so you can manually examine the results.
+    Keep this functionality even after you have all system running, because you sometime want to debug/improve a module.
+
+    :param argv: In case you want to programmatically run this.
+    """
+
+    parser = argparse.ArgumentParser("Test TFL attention mechanism")
+    parser.add_argument('-i', '--image', type=str, help='Path to an image')
+    parser.add_argument("-j", "--json", type=str, help="Path to image json file -> GT for comparison")
+    parser.add_argument('-d', '--dir', type=str, help='Directory to scan images in')
+    args = parser.parse_args(argv)
+
+    # If you entered a custom dir to run from or the default dir exist in your project then:
+    directory_path: Path = Path(args.dir or DEFAULT_BASE_DIR)
+    if directory_path.exists():
+        # gets a list of all the files in the directory that ends with "_leftImg8bit.png".
+        file_list: List[Path] = list(directory_path.glob('*_leftImg8bit.png'))
+
+        for image in file_list:
+            # Convert the Path object to a string using as_posix() method
+            image_path: str = image.as_posix()
+            path: Optional[str] = image_path.replace('_leftImg8bit.png', '_gtFine_polygons.json')
+            image_json_path: Optional[str] = path if Path(path).exists() else None
+            test_find_tfl_lights(image_path, image_json_path)
+
+    if args.image and args.json:
+        test_find_tfl_lights(args.image, args.json)
+    elif args.image:
+        test_find_tfl_lights(args.image)
+    plt.show(block=True)
+
+
+if __name__ == '__main__':
+    main()
